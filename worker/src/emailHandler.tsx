1b86f4f85939484b2451665f13e3922ca2a1669a
sameera-g-mathad <sameeragmathad@gmail.com>
2025-09-08 22:24:40 -0500

    comments: added comments in worker

--- worker/src/emailHandler.tsx
+++ worker/src/emailHandler.tsx
@@ -15,41 +15,64 @@ const sendEmail = async (to: string, template: string, subject: string): Promise
   console.log('Email sent!!')
 };
 
+// Create a redis duplicate to listen to publishing events.
 const redisSub = redisClient.duplicate();
 redisSub.connect();
 
+// Listen to 'signup' event.
 redisSub.subscribe('signup', async (channel, _message) => {
-  const { email, firstName, lastName } = await redisClient.hGetAll(channel);
-  const verificationId = channel.split(':')[2]
-  const template = await getTemplate(
-    <Email
-      firstName={firstName}
-      lastName={lastName}
-      preview="Let’s get started — verify your email to activate your account."
-      mainText={`Thanks for signing up for Parser! We’re excited to have you on board.
+  // Get the details from the redis client.
+  const data = await redisClient.hGetAll(channel);
+  // check if the key is present in the db.
+  if (Object.keys(data).length > 0) {
+    const { email, firstName, lastName } = data;
+
+    // token is assumed to be present at index 2
+    // ex: user:token:<actual_token>
+    const verificationId = channel.split(':')[2]
+    // Get the template for user signup.
+    const template = await getTemplate(
+      <Email
+        firstName={firstName}
+        lastName={lastName}
+        preview="Let’s get started — verify your email to activate your account."
+        mainText={`Thanks for signing up for Parser! We’re excited to have you on board.
             Before we get started, please verify your email to activate your account.
             
             Note: This verification link will expire in 5 minutes.`}
-      extLink={`http://localhost:3050/auth/verify-user/${verificationId}`}
-      linkText=' Verify My Email'
-    />);
-  sendEmail(email!, template, 'Welcome to Parser🎉');
+        extLink={`http://localhost:3050/auth/verify-user/${verificationId}`}
+        linkText=' Verify My Email'
+      />);
+    // send email. By default redis returns string | null for keys.
+    // Forcing variable, that the email will be string.
+    sendEmail(email!, template, 'Welcome to Parser🎉');
+  }
+  else {
+    console.log('Error: No data in redis while sending email to verify signup.')
+  }
 });
 
-
+// This listner if for sending email for resetting passwords
 redisSub.subscribe('forgotPassword', async (channel, _message) => {
-  const { email, firstName, lastName } = await redisClient.hGetAll(channel)
-  const verificationId = channel.split(':')[2]; // channel here is the  token
-  const template = await getTemplate(
-    <Email
-      firstName={firstName}
-      lastName={lastName}
-      preview="Your password reset link is ready"
-      mainText={`We received a request to reset your password. Click the link below to set a new password.
+  // Get the details from the redis client.
+  const data = await redisClient.hGetAll(channel);
+  // check if the key is present in the db.
+  if (Object.keys(data).length > 0) {
+    const { email, firstName, lastName } = data;
+    const verificationId = channel.split(':')[2]; // channel here is the  token
+    const template = await getTemplate(
+      <Email
+        firstName={firstName}
+        lastName={lastName}
+        preview="Your password reset link is ready"
+        mainText={`We received a request to reset your password. Click the link below to set a new password.
                 Note: This password reset link will expire in 2 minutes.
                 `}
-      extLink={`http://localhost:3050/auth/reset-password/${verificationId}`}
-      linkText=' Reset Password'
-    />);
-  sendEmail(email!, template, 'Reset Your Password 🔑');
+        extLink={`http://localhost:3050/auth/reset-password/${verificationId}`}
+        linkText=' Reset Password'
+      />);
+    sendEmail(email!, template, 'Reset Your Password 🔑');
+  } else {
+    console.log('Error: No data in redis while sending email to reset password.')
+  }
 })
--- worker/src/getTemplate.ts
+++ worker/src/getTemplate.ts
@@ -4,5 +4,7 @@ import { render } from '@react-email/components';
 export const getTemplate = async (
   component: React.ReactElement
 ): Promise<string> => {
+  // sends a string equivalent html by taking
+  // in a react component.
   return await render(component);
 };
