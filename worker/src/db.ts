b4a2337a04d19277ec54651caea0848e9aa24569
sameera-g-mathad <sameeragmathad@gmail.com>
2026-02-10 15:45:19 -0600

    worker: delete files in case of unsuccessful uploads

--- worker/src/db.ts
+++ worker/src/db.ts
@@ -18,12 +18,22 @@ export const pg = new Pool({
  */
 export const updateUploads = async (
   id: string,
-  status: 'active' | 'failed'
+  status: 'active' | 'failed',
 ) => {
   await pg.query(
     `UPDATE uploads SET status=$2, updated_at=CURRENT_TIMESTAMP WHERE id=$1`,
-    [id, status]
+    [id, status],
   );
+};
+
+/**
+ * Method to delete the uplaod record if for any reason
+ * embedding or uploading to s3 fails.
+ * This automatically deletes embeddings if any exist.
+ * @param id Id of the upload record
+ */
+export const deleteUpload = async (id: string) => {
+  await pg.query(`DELETE FROM uploads WHERE id=$1`, [id]);
 };
 
 // Create a redis client.
--- worker/src/emailHandler.ts
+++ worker/src/emailHandler.ts
@@ -21,16 +21,16 @@ redisSub.subscribe('signup', async (channel, _message) => {
       `Thanks for signing up for Parser! We’re excited to have you on board.
       Before we get started, please verify your email to activate your account.
       Note: This verification link will expire in 5 minutes.`,
-      `http://localhost:3050/auth/verify-user/${verificationId}`,
+      `${process.env.WEB_URL}/auth/verify-user/${verificationId}`,
       'Verify My Email',
-      '#f472b6' // color to use in the email.
+      '#f472b6', // color to use in the email.
     );
     // send email. By default redis returns string | null for keys.
     // Forcing variable, that the email will be string.
     sendEmail(email!, template, 'Welcome to Parser🎉');
   } else {
     console.log(
-      'Error: No data in redis while sending email to verify signup.'
+      'Error: No data in redis while sending email to verify signup.',
     );
   }
 });
@@ -49,15 +49,15 @@ redisSub.subscribe('forgotPassword', async (channel, _message) => {
       'Your password reset link is ready',
       `We received a request to reset your password. Click the link below to set a new password.
       Note: This password reset link will expire in 5 minutes.`,
-      `http://localhost:3050/auth/reset-password/${verificationId}`,
+      `${process.env.WEB_URL}/auth/reset-password/${verificationId}`,
       'Reset Password',
-      '#fb923c'
+      '#fb923c',
     );
 
     sendEmail(email!, template, 'Reset Your Password 🔑');
   } else {
     console.log(
-      'Error: No data in redis while sending email to reset password.'
+      'Error: No data in redis while sending email to reset password.',
     );
   }
 });
